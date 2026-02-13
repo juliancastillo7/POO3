9398e69c017760fa5638ba27a3cba4ae6f94cdc9
julian castillo <juliancas7@outlook.com>
2026-02-13 17:21:53 -0500

    Cambios

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
         System.out println("JULIAN DAVID CASTILLO POVEDA --- PROGRAMACION EN JAVA"):
+
+        System.out.println("1. COMANDOS DE GIT VISTOS");
+        System.out .println("1. git. add ");
+        System.out .println("2. git commit -m");
+        System.out .println("3. git push");
+        System.out .println("4. git switch");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
